7a447b50a459c68d110a241eeeefa064e3a49a7d
ugursaricam <ugursrcm@gmail.com>
2023-07-23 02:52:59 +0300

    created file and uploaded dataset

---
+++ forecasting_passengers.py
@@ -0,0 +1,15 @@
+import itertools
+import warnings
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+from sklearn.metrics import mean_absolute_error
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+from statsmodels.tsa.holtwinters import SimpleExpSmoothing
+from statsmodels.tsa.seasonal import seasonal_decompose
+import statsmodels.tsa.api as smt
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+warnings.filterwarnings('ignore')
+
