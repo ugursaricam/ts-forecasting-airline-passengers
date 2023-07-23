03ec2dcb4c075cd895b74dbefb84c0c312bead1f
ugursaricam <ugursrcm@gmail.com>
2023-07-23 03:09:21 +0300

    created model

--- forecasting_passengers.py
+++ forecasting_passengers.py
@@ -13,3 +13,98 @@ import statsmodels.tsa.api as smt
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 warnings.filterwarnings('ignore')
 
+df = pd.read_csv("datasets/airline-passengers.csv", parse_dates=True)
+
+df.head()
+
+df.info()
+
+df.isnull().sum()
+
+df["month"] = pd.to_datetime(df["month"])
+
+df = df.set_index("month").squeeze()
+
+def ts_decompose(dataframe, model="additive", stationary=False):
+    result = seasonal_decompose(dataframe, model=model)
+    fig, axes = plt.subplots(4, 1, sharex=True, sharey=False)
+    fig.set_figheight(10)
+    fig.set_figwidth(15)
+
+    axes[0].set_title("Decomposition for " + model + " model")
+    axes[0].plot(dataframe, 'k', label='Original ' + model)
+    axes[0].legend(loc='upper left')
+
+    axes[1].plot(result.trend, label='Trend')
+    axes[1].legend(loc='upper left')
+
+    axes[2].plot(result.seasonal, 'g', label='Seasonality & Mean: ' + str(round(result.seasonal.mean(), 4)))
+    axes[2].legend(loc='upper left')
+
+    axes[3].plot(result.resid, 'r', label='Residuals & Mean: ' + str(round(result.resid.mean(), 4)))
+    axes[3].legend(loc='upper left')
+    plt.show(block=True)
+
+ts_decompose(df)
+
+df.shape
+
+train = df[:115]
+test = df[115:]
+
+model = SARIMAX(train, order=(1, 1, 1), seasonal_order=(0, 0, 0, 12))
+sarima_model = model.fit(disp=0)
+
+test.shape
+
+y_pred_test = sarima_model.get_forecast(steps=29)
+
+y_pred = y_pred_test.predicted_mean
+
+type(y_pred)
+
+def plot_co2(train, test, y_pred, title):
+    mae = mean_absolute_error(test, y_pred)
+    train["1985":].plot(legend=True, label="TRAIN", title=f"{title}, MAE: {round(mae,2)}")
+    test.plot(legend=True, label="TEST", figsize=(6, 4))
+    y_pred.plot(legend=True, label="PREDICTION")
+    plt.show(block=True)
+
+plot_co2(train, test, y_pred, "SARIMA")
+
+p = d = q = range(0, 2)
+pdq = list(itertools.product(p, d, q))
+seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+
+def sarima_optimizer_aic(train, pdq, seasonal_pdq):
+    best_aic, best_order, best_seasonal_order = float("inf"), None, None
+    for param in pdq:
+        for param_seasonal in seasonal_pdq:
+            try:
+                sarimax_model = SARIMAX(train, order=param, seasonal_order=param_seasonal)
+                results = sarimax_model.fit(disp=0)
+                aic = results.aic
+                if aic < best_aic:
+                    best_aic, best_order, best_seasonal_order = aic, param, param_seasonal
+                print('SARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, aic))
+            except:
+                continue
+    print('SARIMA{}x{}12 - AIC:{}'.format(best_order, best_seasonal_order, best_aic))
+    return best_order, best_seasonal_order
+
+best_order, best_seasonal_order = sarima_optimizer_aic(train, pdq, seasonal_pdq)
+
+best_order #(1, 1, 0)
+best_seasonal_order #(0, 1, 0, 12)
+
+model = SARIMAX(train, order=best_order, seasonal_order=best_seasonal_order)
+sarima_final_model = model.fit(disp=0)
+y_pred_test = sarima_final_model.get_forecast(steps=29)
+
+y_pred = y_pred_test.predicted_mean
+
+plot_co2(train, test, y_pred, "SARIMA")
+
+
+
+
